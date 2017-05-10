99eaf10bfe7f9175a147d5e2e321a61848413891
dzysghr <dzysghr@live.com>
2017-05-10 21:09:39 +0800

    完善注销

--- src/main/java/com/dzy/onedriveclient/module/more/MoreFragment.java
+++ src/main/java/com/dzy/onedriveclient/module/more/MoreFragment.java
@@ -1,6 +1,8 @@
 package com.dzy.onedriveclient.module.more;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
@@ -14,8 +16,11 @@ import com.dzy.onedriveclient.model.drive.Drive;
 import com.dzy.onedriveclient.model.drive.IUserModel;
 import com.dzy.onedriveclient.module.WelcomeActivity;
 import com.dzy.onedriveclient.utils.RxHelper;
+import com.dzy.onedriveclient.utils.UserInfoSPUtils;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.util.Locale;
 
 import io.reactivex.annotations.NonNull;
@@ -55,6 +60,39 @@ public class MoreFragment extends BaseFragment {
                 switchAccount();
             }
         });
+        mLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                requestLogout();
+            }
+        });
+    }
+
+    private void requestLogout(){
+        new AlertDialog.Builder(getContext())
+                .setMessage("确定要注销帐号吗?")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        logout();
+                    }
+                }).show();
+    }
+
+    private void logout(){
+        ModelFactory.getDBModel().close();
+        File db = new File(getContext().getFilesDir().getParent(), "databases");
+        final String user = UserInfoSPUtils.getUser();
+        File[] list = db.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.equals(user)||name.equals(user+"-journal");
+            }
+        });
+        for (File i:list){
+            i.delete();
+        }
+        switchAccount();
     }
 
     private void switchAccount(){
